9edba523c88d497eb7b42f6d6812793590df7ec6
Vladimir Prin <69980366+AstraL13666@users.noreply.github.com>
2023-08-02 18:47:43 +0300

    Update config.py
    =)

--- config_data/config.py
+++ config_data/config.py
@@ -2,5 +2,5 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 
-bot = Bot(token='5990241109:AAFCF5fvWCkWYc8JnV8tKK7GnSP-Y602A3I')
+bot = Bot(token='pass')
 dp = Dispatcher(storage=MemoryStorage())
